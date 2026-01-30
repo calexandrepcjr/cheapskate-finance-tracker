a34193d696cfb9b25de87a7f110a120805cc233d
Carlos Alexandre Pires de Carvalho Junior <capcj@protonmail.com>
2026-01-30 19:30:44 -0300

    feat: improve dashboard UX, add transaction deletion, and enhance parser

--- server/routes.go
+++ server/routes.go
@@ -8,4 +8,5 @@ func (app *Application) setupRoutes(r chi.Router) {
 	r.Get("/", app.HandleHome)
 	r.Get("/dashboard", app.HandleDashboard)
 	r.Post("/api/transaction", app.HandleTransactionCreate)
+	r.Delete("/api/transaction/{id}", app.HandleTransactionDelete)
 }
